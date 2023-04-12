cb3e531bf352da1ded973a9c4dbe427dd6cffa59
Ellen Gomes <ellen_gomes14@hotmail.com>
2023-04-12 11:01:46 -0300

    reformating data

--- user_list_backend/controllers/users.js
+++ user_list_backend/controllers/users.js
@@ -5,9 +5,9 @@ const getUsers = async (request, response, next) => {
 
         const data = results.map(item => {
             return {
-                name: `${item.name.title} ${item.name.first} ${item.name.last}`,
-                gender: item.gender,
-                location: `${item.location.street.name}, ${item.location.street.number} - ${item.location.city}, ${item.location.state}, ${item.location.country} - ${item.location.postcode}`,
+                name: `${item.name.first} ${item.name.last}`,
+                gender: item.gender === "female" ? "Feminino" : "Masculino",
+                location: `${item.location.city}, ${item.location.state}, ${item.location.country} - ${item.location.postcode}`,
                 email: item.email,
                 age: item.dob.age,
                 registered: item.registered.date.split("T")[0],
